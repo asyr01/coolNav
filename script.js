b4b57182d1938ffd7ed3696077b1d61226d1dc79
example <alisayar0107@gmail.com>
2021-01-25 14:47:42 +0300

    event listeners added for toggling

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const nav2 = document.getElementsById('nav-2');
 const nav3 = document.getElementsById('nav-3');
 const nav4 = document.getElementsById('nav-4');
 const nav5 = document.getElementsById('nav-5');
+
+// Event Listeners
+menuBars.addEventListener('click', toggleNav);
+nav1.addEventListener('click', toggleNav);
+nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
+nav4.addEventListener('click', toggleNav);
+nav5.addEventListener('click', toggleNav);
