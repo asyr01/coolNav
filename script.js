18e86a83a1a6532b1c1c6f1345fbf01d6121efe6
example <alisayar0107@gmail.com>
2021-01-25 15:01:38 +0300

    now hamburger is toggling

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const menuBars = document.getElementById('menu-bars');
 const overlay = document.getElementById('overlay');
-const nav1 = document.getElementsById('nav-1');
-const nav2 = document.getElementsById('nav-2');
-const nav3 = document.getElementsById('nav-3');
-const nav4 = document.getElementsById('nav-4');
-const nav5 = document.getElementsById('nav-5');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
+
+function toggleNav() {
+    // Toggle: Menu Bars Open/Closed
+    menuBars.classList.toggle('change');
+}
 
 // Event Listeners
 menuBars.addEventListener('click', toggleNav);
