6b94f660d1188cfabcb6a4ae9a0cbcdec9d2d77b
example <alisayar0107@gmail.com>
2021-01-25 14:20:07 +0300

    constants added to the script

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementsById('nav-1');
+const nav2 = document.getElementsById('nav-2');
+const nav3 = document.getElementsById('nav-3');
+const nav4 = document.getElementsById('nav-4');
+const nav5 = document.getElementsById('nav-5');
