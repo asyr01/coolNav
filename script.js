c48edd342f7b10cf4ffe7640e12cd1de846a18fe
example <alisayar0107@gmail.com>
2021-01-27 15:09:19 +0300

    we will create a function called navAnimation for getting rid of all not dry code

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
 
 function toggleNav() {
     // Toggle: Menu Bars Open/Closed
@@ -16,30 +17,12 @@ function toggleNav() {
      overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
      
     // Animate in - Nav Items
-    nav1.classList.remove('slide-in-1');
-    nav1.classList.remove('slide-out-1');
-    nav2.classList.add('slide-in-2');
-    nav2.classList.remove('slide-out-2');
-    nav3.classList.add('slide-in-3');
-    nav3.classList.remove('slide-out-3');
-    nav4.classList.add('slide-in-4');
-    nav4.classList.remove('slide-out-4');
-    nav5.classList.add('slide-in-5');
-    nav5.classList.remove('slide-out-5');
+      navAnimation('out', 'in');
     } else {
     // Animate Out - overlay
     overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
     // Animate Out- Nav Items
-    nav1.classList.remove('slide-out-1');
-    nav1.classList.remove('slide-in-1');
-    nav2.classList.add('slide-out-2');
-    nav2.classList.remove('slide-in-2');
-    nav3.classList.add('slide-out-3');
-    nav3.classList.remove('slide-in-3');
-    nav4.classList.add('slide-out-4');
-    nav4.classList.remove('slide-in-4');
-    nav5.classList.add('slide-out-5');
-    nav5.classList.remove('slide-in-5');
+      navAnimation('in', 'out');  
     }
 }
 
